c74895b70ca295e0a8ad7fddaea6f0add9ef90c0
Akshat-6261799795 <akshatrahangdale31@gmail.com>
2025-12-26 12:55:29 +0530

    Refactor sumOfDigits function and main method
    Removed redundant code and improved formatting.

--- Practical 8/1.c
+++ Practical 8/1.c
@@ -1,11 +1,27 @@
-int sumOfDigits(int n) {
-    int sum = 0;
-    printf("Name: Akshat Rahangdale\nErp Id: 10133\n");
+#include <stdio.h>
 
-    while (n != 0) {
+int sumOfDigits(int n)
+{
+    int sum = 0;
+    while (n != 0)
+    {
         sum = sum + (n % 10);
         n = n / 10;
     }
-
     return sum;
 }
+
+int main()
+{
+    int n;
+    printf("Name: Akshat Rahangdale\nErp Id: 10133\n");
+    scanf("%d", &n);
+    printf("Sum of digits = %d", sumOfDigits(n));
+    return 0;
+}
+
+// Output of the code
+// Name: Akshat Rahangdale
+// Erp Id: 10133
+// 5643
+// Sum of digits = 18
