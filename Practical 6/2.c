9e07f2f50db205d4c870cd9ed1670d750a94e402
Akshat-6261799795 <akshatrahangdale31@gmail.com>
2025-12-26 12:13:02 +0530

    Add files via upload

--- Practical 6/2.c
+++ Practical 6/2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int a, b;
+    int a, b, sum = 0;
     printf("Name: Akshat Rahangdale\nErp Id: 10133\n");
 
     printf("Enter starting number: ");
@@ -11,8 +11,13 @@ int main() {
     scanf("%d", &b);
 
     for (int i = a; i <= b; i++) {
-        printf("%d ", i);
+        if (i % 2 != 0)
+            continue;
+
+        sum = sum + i;
     }
+
+    printf("Sum of even numbers = %d", sum);
 
     return 0;
 }
