e5b577af50fbb6441df9889ff5cac02b8c381d25
Akshat-6261799795 <akshatrahangdale31@gmail.com>
2025-12-29 15:02:10 +0530

    Update 1.c

--- Practical 9/1.c
+++ Practical 9/1.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main() {
+    int arr[5] = {4, 3, 2, 1, 5};
+    int i, temp;
+    int n = 5;
+
+    for (i = 0; i < n / 2; i++) {
+        temp = arr[i];
+        arr[i] = arr[n - 1 - i];
+        arr[n - 1 - i] = temp;
+    }
+
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
+
+// Output for this code 
+// 5 1 2 3 4
