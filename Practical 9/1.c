4f8cd857420ea8ff698c61ffc1281d0b8e822b4d
Akshat-6261799795 <akshatrahangdale31@gmail.com>
2025-12-29 15:10:10 +0530

    Update 1.c

--- Practical 9/1.c
+++ Practical 9/1.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    int arr[5] = {4, 3, 2, 1, 5};
-    int i, temp;
-    int n = 5;
+    int n, i, temp;
+    int arr[100];
+    printf("Name- Akshat Rahangdale\nErp- 10133\n");
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
 
     for (i = 0; i < n / 2; i++) {
         temp = arr[i];
@@ -11,6 +19,7 @@ int main() {
         arr[n - 1 - i] = temp;
     }
 
+    printf("Reversed array:\n");
     for (i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
@@ -18,5 +27,15 @@ int main() {
     return 0;
 }
 
-// Output for this code 
-// 5 1 2 3 4
+// Output for the code 
+// Name- Akshat Rahangdale
+// Erp- 10133
+// Enter number of elements: 4
+// Enter 4 elements:
+// 3
+// 4
+// 3
+// 2
+// Reversed array:
+// 2 3 4 3 
+
